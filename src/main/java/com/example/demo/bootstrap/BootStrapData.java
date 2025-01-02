146fa859e4c9dbca8bc40eaefea4edb9524135a4
Anas Nasir <anasnaasir1@gmail.com>
2025-01-01 19:56:45 -0500

    Products and Parts list

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -60,18 +60,52 @@ public class BootStrapData implements CommandLineRunner {
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
 
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
+/*
+        Product HighPerformanceGamingPcs= new Product("HighPerformanceGamingPcs",100.0,15);
+        Product StreamingSetup= new Product("StreamingSetup",100.0,15);
+        productRepository.save( HighPerformanceGamingPcs);
+        productRepository.save(StreamingSetup);
+*/
 
+
+        if (partRepository.count() == 0 && productRepository.count() == 0) {
+            Part part1 = new Part("AMD Ryzen 9 5900X Processor", 299.99, 10);
+            Part part2 = new Part("NVIDIA RTX 4090 GPU", 499.99, 5);
+            Part part3 = new Part("64GB Corsair Vengeance RAM", 99.99, 20);
+            Part part4 = new Part("1TB NVMe SSD", 129.99, 15);
+            Part part5 = new Part("850W PSU (Power Supply Unit)", 199.99, 8);
+
+            partRepository.save(part1);
+            partRepository.save(part2);
+            partRepository.save(part3);
+            partRepository.save(part4);
+            partRepository.save(part5);
+
+
+
+            Product product1 = new Product("Elgato Stream Deck", 1499.99, 3);
+            Product product2 = new Product("Logitech C920 Webcam", 399.99, 7);
+            Product product3 = new Product("Blue Yeti Microphone", 1299.99, 4);
+            Product product4 = new Product("Ring Light", 799.99, 6);
+            Product product5 = new Product("Green Screen", 999.99, 5);
+
+            productRepository.save(product1);
+            productRepository.save(product2);
+            productRepository.save(product3);
+            productRepository.save(product4);
+            productRepository.save(product5);
+
+        }
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
         System.out.println(productRepository.findAll());
         System.out.println("Number of Parts"+partRepository.count());
         System.out.println(partRepository.findAll());
+        }
+
+
 
     }
-}
+
+
+
